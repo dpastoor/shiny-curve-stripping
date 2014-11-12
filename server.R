7b7902c20dfc7e9889c079dcc64845b8ce8c5235
Devin Pastoor <devin.pastoor@gmail.com>
2014-11-12 16:49:13 -0500

    data upload and then plot with selection of x and y axes working

--- server.R
+++ server.R
@@ -4,12 +4,30 @@
 # 
 # http://www.rstudio.com/shiny/
 #
-
+ui_data <- Theoph
 library(shiny)
+library(ggplot2)
+library(PKPDmisc)
+shinyServer(function(input, output, session) {
 
-shinyServer(function(input, output) {
+  
+  
+  dataset <- reactive({
+    inFile <- input$file1
+    if(is.null(inFile)) return(NULL)  
+    if(!is.null(inFile)) {
+      if(input$has_units) {
+        data <-  read_csv_wunits(inFile$datapath, sep=input$sep, 
+                                 quote=input$quote)
+      } else {
+        data <-  read.csv(inFile$datapath, header=input$has_header, sep=input$sep, 
+                          quote=input$quote)
+      }
+      return(data)
+    }
+  })
+  
   output$contents <- renderTable({
-    
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
     # 'size', 'type', and 'datapath' columns. The 'datapath'
@@ -17,17 +35,50 @@ shinyServer(function(input, output) {
     # be found.
     
     inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    if(input$has_units) {
-      read_csv_wunits(inFile$datapath, sep=input$sep, 
-               quote=input$quote)
+    if(is.null(inFile)) return(NULL)  
+    if(!is.null(inFile)) {
+      if(input$has_units) {
+        data <-  read_csv_wunits(inFile$datapath, sep=input$sep, 
+                                 quote=input$quote)
       } else {
-        read.csv(inFile$datapath, header=input$has_header, sep=input$sep, 
-            quote=input$quote)
-  }
+        data <-  read.csv(inFile$datapath, header=input$has_header, sep=input$sep, 
+                          quote=input$quote)
+      }
+      return(data)
+    }
+
+    
+    
+    
+  })
+  
+  observe({
+    #not positive if observed needed but based on googling around and it works
+    # http://stackoverflow.com/questions/20073948/observe-updateselectinput-based-on-first-selection
+    if(!is.null(dataset)) {
+      updateSelectInput(session, 'x', 'X', names(dataset()), "TIME" )
+      updateSelectInput(session, 'y', 'Y', names(dataset()), "COBS")
+    }
+  })
+
+
+  
+  output$plot <- renderPlot({
+    if(is.null(dataset)) return(NULL)
+    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y))
+    
+    if (input$point)
+      p <- p + geom_point(size = input$point_size)
+    if (input$line)
+      p <- p + geom_line(size = input$line_size)
+      
+    if (input$log_y)
+      p <- p + scale_y_log10()
+    
+      
+    print(p + base_theme_obs())
+   
   })
   
 })
+
--- ui.R
+++ ui.R
@@ -2,33 +2,60 @@
 library(shiny)
 library(ggplot2)
 library(PKPDmisc)
-
 shinyUI(navbarPage("Navbar",
   tabPanel("Upload Dataset",
-  sidebarLayout(
-    sidebarPanel(
-      fileInput('file1', 'Choose CSV File',
-                accept=c('text/csv', 
-                         'text/comma-separated-values,text/plain', 
-                         '.csv')),
-      tags$hr(),
-      checkboxInput('has_header', 'Header', TRUE),
-      checkboxInput('has_units', 'Has Units', TRUE),
-      radioButtons('sep', 'Separator',
-                   c(Comma=',',
-                     Semicolon=';',
-                     Tab='\t'),
-                   ','),
-      radioButtons('quote', 'Quote',
-                   c(None='',
-                     'Double Quote'='"',
-                     'Single Quote'="'"),
-                   '"')
-    ),
-    mainPanel(
-      tableOutput('contents')
+    sidebarLayout(
+      sidebarPanel(
+        fileInput('file1', 'Choose CSV File',
+                  accept=c('text/csv', 
+                           'text/comma-separated-values,text/plain', 
+                           '.csv')),
+        tags$hr(),
+        checkboxInput('has_header', 'Header', TRUE),
+        checkboxInput('has_units', 'Has Units', TRUE),
+        radioButtons('sep', 'Separator',
+                     c(Comma=',',
+                       Semicolon=';',
+                       Tab='\t'),
+                     ','),
+        radioButtons('quote', 'Quote',
+                     c(None='',
+                       'Double Quote'='"',
+                       'Single Quote'="'"),
+                     '"')
+      ),
+      mainPanel(
+        tableOutput('contents')
+      )
     )
-  )
-))
+  ),
+  tabPanel("Initial Estimates",
+           fluidPage(
+             titlePanel("Test"),
+               plotOutput("plot"),
+               fluidRow(
+                 column(3,
+                        h4("PKPD Explorer"),
+                        checkboxInput('point', 'Point', value = TRUE),
+                        checkboxInput('line', 'Line'),
+                        checkboxInput('log_y', 'log Y scale', value = FALSE),
+                        sliderInput('point_size', 'Point Size', 
+                                    min=0.5, max=5,
+                                    value=2, 
+                                    step=0.2),
+                        sliderInput('line_size', 'Line Size', 
+                                    min=0.5, max=5,
+                                    value=1, 
+                                    step=0.2)
+                 ),
+                 column(4, offset = 1,
+                        selectInput('x', 'X', names(ui_data), "TIME" ),
+                        selectInput('y', 'Y', names(ui_data), "COBS")
+                 )
+               )
+             )
+          
+)
+)
 )
 
